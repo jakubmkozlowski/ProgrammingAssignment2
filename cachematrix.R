692ad9fcb676b64689c597c31f1ff627875e35d6
jakubmkozlowski <jakub.kozlowski@gmail.com>
2015-01-23 09:41:20 +0100

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions allow for retrieving cached values of calculations.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix() creates a data and result pair construct.
+makeCacheMatrix <- function(m = matrix()) {
+    inv <- NULL
+    set <- function(x) {
+        m <<- x
+        inv <<- NULL
+    }
+    get <- function() m
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## cacheSolve() looks up the result, if it's not available, it calculates
+## and stores the result; otherwise it just returns the result.
+cacheSolve <- function(cm, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- cm$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- cm$get()
+    inv <- solve(data, ...)
+    cm$setinverse(inv)
+    inv
 }
